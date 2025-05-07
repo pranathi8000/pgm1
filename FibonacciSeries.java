94aba6c1a45dcb9f96a2ae68ce5de4c2a9461bcd
shivhanchin07 <ravihanchin@gmail.com>
2025-05-07 11:32:34 +0530

    student

---
+++ FibonacciSeries.java
@@ -0,0 +1,16 @@
+package pgm1;
+
+public class FibonacciSeries {
+    public static void main(String[] args) {
+        int n = 10; // Number of terms in the series
+        int first = 0, second = 1;
+
+        System.out.println("Fibonacci Series up to " + n + " terms:");
+        for (int i = 1; i <= n; i++) {
+            System.out.print(first + " ");
+            int next = first + second;
+            first = second;
+            second = next;
+        }
+    }
+}
